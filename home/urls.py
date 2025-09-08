2409625afa4c3db7d09c091c2ac03958285c21bd
Sadra <pourezzatsadra@gmail.com>
2025-09-08 16:53:41 +0330

    The home app views
    The home app views are modified and those are got online!

--- home/urls.py
+++ home/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
+from home import views
 
 app_name = 'home'
 
 urlpatterns = [
-    
+    path('', views.IndexView.as_view(), name='home_page'),
+    path('contact', views.ContactView.as_view(), name='contact_page'),
+    path('about', views.AboutView.as_view(), name='about_page'),
 ]
