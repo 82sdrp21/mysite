993db6efed6482778000552b21fb4d96c7a24493
Sadra <pourezzatsadra@gmail.com>
2025-09-10 11:02:40 +0330

    The category filter view implemented

--- blog/urls.py
+++ blog/urls.py
@@ -5,5 +5,6 @@ app_name = 'blog'
 
 urlpatterns = [
     path('', views.BlogListView.as_view(), name='blog-list'),
+    path('cat/<str:cat>', views.CategoryListView.as_view(), name='blog-list'),
     path('<slug:slug>', views.BlogDetailView.as_view(), name='blog-detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, get_list_or_404
 from django.views.generic import ListView, DetailView
-from blog.models import Post
+from blog.models import Post, Category
 
 class BlogListView(ListView):
     template_name = 'blog/blog-list.html'
     model = Post
-    queryset = Post.objects.filter(status=True)
+    queryset = Post.objects.filter(status=1)
     context_object_name = 'posts'
+
+class CategoryListView(ListView):
+    template_name = 'blog/blog-list.html'
+    model = Post
+    context_object_name = 'posts'
+
+    def get_queryset(self, *args, **kwargs):
+        return Post.objects.filter(categories__name=self.kwargs.get('cat'))
+        
+
 
 class BlogDetailView(DetailView):
     template_name = 'blog/blog-detail.html'
