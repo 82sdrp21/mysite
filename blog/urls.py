cb355c0874e64c5fc48b9e09b7b15f49a7d6b197
Sadra <pourezzatsadra@gmail.com>
2025-09-10 16:51:36 +0330

    Category + Author + Search filter
    In this commition the category + author + search filtering views are implemented using the inheritance from the original BlogListView via manpulating the queryset!

--- blog/urls.py
+++ blog/urls.py
@@ -5,6 +5,8 @@ app_name = 'blog'
 
 urlpatterns = [
     path('', views.BlogListView.as_view(), name='blog-list'),
-    path('cat/<str:cat>', views.CategoryListView.as_view(), name='blog-list'),
+    path('cat/<str:cat>', views.CategoryListView.as_view(), name='blog-list-category-filtered'),
+    path('author/<str:username>', views.AuthorListView.as_view(), name='blog-list-author-filtered'),
+    path('search', views.SearchListView.as_view(), name='blog-list-search'),
     path('<slug:slug>', views.BlogDetailView.as_view(), name='blog-detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,21 +1,38 @@
 from django.shortcuts import get_object_or_404, get_list_or_404
 from django.views.generic import ListView, DetailView
 from blog.models import Post, Category
+from django.http import Http404
 
 class BlogListView(ListView):
+
     template_name = 'blog/blog-list.html'
     model = Post
     queryset = Post.objects.filter(status=1)
     context_object_name = 'posts'
+    paginate_by = 2
+    
 
-class CategoryListView(ListView):
-    template_name = 'blog/blog-list.html'
-    model = Post
-    context_object_name = 'posts'
-
+class CategoryListView(BlogListView):
+    
     def get_queryset(self, *args, **kwargs):
-        return Post.objects.filter(categories__name=self.kwargs.get('cat'))
+        return Post.objects.filter(categories__name=self.kwargs.get('cat'), status=1)
         
+
+class AuthorListView(BlogListView):
+    
+    def get_queryset(self, *args, **kwargs):
+        return Post.objects.filter(author__username__exact=self.kwargs.get('username'), status=1)
+    
+class SearchListView(BlogListView):
+    paginate_by = None
+    
+    def get_queryset(self, *args, **kwargs):
+
+        if search_word := self.request.GET.get('s'):
+            posts = Post.objects.filter(title__icontains=search_word, content__icontains=search_word, status=1)
+            return posts
+        else:
+            raise Http404
 
 
 class BlogDetailView(DetailView):
